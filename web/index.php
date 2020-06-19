4415ccc3c17290cc7fe1d7a576d8cee5fa7e2598
Miroslav Nikolov <m.nikolov@seeburger.com>
2020-06-19 15:41:48 +0300

    Update HTML page

--- web/index.php
+++ web/index.php
@@ -4,15 +4,52 @@
 </head>
 <body>
   <div align="center">
-      <img src="vbox_gradient.png"><img src="vbox_logodown.png">
-      <img src="vagrant.svg">
-      <img src="docker.jpg">
-      <img src="ansible-redhat.png">
-      <img src="jenkins.svg">
-
-    <h1>Welcome to the world of Jenkins!</h1>
-    <h2>There is plenty to explore here...</h2>
-    <h2>You should be both patient and curious ... :)</h2>
+      <table>
+          <tr>
+              <td width="50%"><img src="vbox_gradient.png"><img src="vbox_logodown.png"></td>
+              <td style="vertical-align: top;"><strong>VirtualBox</strong> is a powerful x86 and AMD64/Intel64 virtualization product for
+              enterprise as well as home use. Not only is VirtualBox an extremely feature rich, high performance product for enterprise
+              customers, it is also the only professional solution that is freely available as Open Source Software under the terms of
+              the GNU General Public License (GPL) version 2. See "About VirtualBox" for an introduction.<br/>
+              Presently, VirtualBox runs on Windows, Linux, Macintosh, and Solaris hosts and supports a large number of guest operating
+              systems including but not limited to Windows (NT 4.0, 2000, XP, Server 2003, Vista, Windows 7, Windows 8, Windows 10),
+              DOS/Windows 3.x, Linux (2.4, 2.6, 3.x and 4.x), Solaris and OpenSolaris, OS/2, and OpenBSD.<br/>
+              VirtualBox is being actively developed with frequent releases and has an ever growing list of features, supported guest
+              operating systems and platforms it runs on. VirtualBox is a community effort backed by a dedicated company: everyone is
+              encouraged to contribute while Oracle ensures the product always meets professional quality criteria.
+              </td>
+          </tr>
+          <tr>
+              <td width="50%"><img src="vagrant.svg"></td>
+              <td style="vertical-align: top;"><strong>Vagrant</strong> - Development Environments Made Easy<br/>
+              Vagrant is a tool for building and managing virtual machine environments in a single workflow. With an easy-to-use workflow
+              and focus on automation, Vagrant lowers development environment setup time, increases production parity, and makes
+              the "works on my machine" excuse a relic of the past.
+              </td>
+              </tr>
+          <tr>
+              <td width="50%"><img src="docker.jpg"></td>
+              <td style="vertical-align: top;"><strong>Docker</strong>
+              Developing apps today requires so much more than writing code. Multiple languages, frameworks, architectures, and
+              discontinuous interfaces between tools for each lifecycle stage creates enormous complexity. Docker simplifies and
+              accelerates your workflow, while giving developers the freedom to innovate with their choice of tools, application stacks,
+              and deployment environments for each project.
+              </td>
+          </tr>
+          <tr>
+              <td width="50%"><img src="ansible-redhat.png"></td>
+              <td style="vertical-align: top;"><strong>Ansible</strong> by RedHat<br/>
+              The enterprise solution for building and operating automation at scale
+              </td>
+          </tr>
+          <tr>
+              <td width="50%"><img src="jenkins.svg"></td>
+              <td style="vertical-align: top;"><strong>Jenkins</strong> Build great things at any scale<br/>
+              The leading open source automation server, Jenkins provides hundreds of plugins to support building, deploying and automating
+              any project.
+              </td>
+          </tr>
+      </table>
   </div>
 
   Running on <b><?php echo gethostname(); ?></b>
