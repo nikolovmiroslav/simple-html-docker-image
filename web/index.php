1ba47510a09ed067ca5731f444a8089c4f13cf4c
Miroslav Nikolov <m.nikolov@seeburger.com>
2020-06-19 16:01:40 +0300

    Remove second virtualbox image

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@
       <h1>DevOps primary members</h1>
       <table>
           <tr>
-              <td width="30%" height="50%"><img src="vbox_gradient.png"><img src="vbox_logodown.png"></td>
+              <td width="30%" height="50%"><img src="vbox_gradient.png"></td>
               <td style="vertical-align: top;"><strong>VirtualBox</strong> is a powerful x86 and AMD64/Intel64 virtualization product for
               enterprise as well as home use. Not only is VirtualBox an extremely feature rich, high performance product for enterprise
               customers, it is also the only professional solution that is freely available as Open Source Software under the terms of
